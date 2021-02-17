91ff8d7066f8fd156d17383b6668fb23b4a470b2
Toriku <anatoly.kravtsov@gmail.com>
2021-02-17 13:46:35 +0500

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+        number = String(Math.abs(n)).split("").reverse();
+        output = "";
+        for (letter of number) {
+            output += letter;
+        }
+        return Number(output);
 }
